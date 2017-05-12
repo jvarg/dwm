4431765696061d9b9eabb51c6583c64240ead5d8
jwagner <johannes.wagner@openmailbox.org>
2017-05-12 08:36:43 +0200

    changed the font size of terminus

--- config.h
+++ config.h
@@ -7,10 +7,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 static const char *fonts[] = {
-  "Terminus:size=14",
+  "Terminus:pixelsize=18:antialias=true:autohint=true",
 };
 
-static const char dmenufont[]       = "-*-terminus-medium-r-*-*-24-*-*-*-*-*-*-*";
+static const char dmenufont[]       = "Terminus:pixelsize=18:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -69,7 +69,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
-static const char *xlock[]  = { "xlock", NULL };
+static const char *xlock[]  = { "slock", NULL };
+static const char *filemanage[]  = { "pcmanfm", NULL };
 static const char *urlcmd[]  = { "clipmenu-url", NULL };
 static const char *clipcmd[]  = { "clipmenu", "-fn", dmenufont, NULL };
 static const char *cmdsoundup[]  = { "amixer", "-q", "sset", "Master", "5%+", NULL };
@@ -83,6 +84,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = xlock } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = filemanage } },
   { MODKEY,                       XK_Insert, spawn,          {.v = clipcmd } },
   { MODKEY,                       XK_o,      spawn,          {.v =  urlcmd } },
   { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
